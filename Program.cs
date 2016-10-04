35fa77d4dd10059f026c4e7d2225f98a81f9a7f9
Hudson Prestidge <charcon@ihug.co.nz>
2016-10-04 15:37:34 +1300

    main menu working

--- Program.cs
+++ Program.cs
@@ -6,7 +6,34 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
+            MainMenu();
+        }
 
+        private static void MainMenu() 
+        {
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Option 1");
+            Console.WriteLine("2) Option 2");
+            Console.WriteLine("3) Exit");
+            string result = Console.ReadLine();
+            if(result == "1"){
+                PrintNumbers();
+            } else if (result == "2"){
+                GuessingGame();
+            } else if (result == "3"){
+
+            } else {
+                Console.WriteLine("Invalid input");
+            }
+        }
+
+        private static void PrintNumbers() {
+            Console.WriteLine("Print Numbers");
+            Console.ReadLine();
+        }
+        private static void GuessingGame() {
+            Console.WriteLine("Guessing game!");
+            Console.ReadLine();
         }
     }
 }
