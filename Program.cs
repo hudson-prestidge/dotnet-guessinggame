e234d2daa436abc10a5dd3ad65d2caecf1c99159
Hudson Prestidge <charcon@ihug.co.nz>
2016-10-04 15:56:43 +1300

    printing numbers game works

--- Program.cs
+++ Program.cs
@@ -14,30 +14,40 @@ namespace ConsoleApplication
 
         private static bool MainMenu() 
         {
+            Console.Clear();
             Console.WriteLine("Choose an option:");
-            Console.WriteLine("1) Option 1");
-            Console.WriteLine("2) Option 2");
+            Console.WriteLine("1) Printing Numbers");
+            Console.WriteLine("2) Guessing Game");
             Console.WriteLine("3) Exit");
             string result = Console.ReadLine();
             if(result == "1"){
                 PrintNumbers();
-                return true;
             } else if (result == "2"){
                 GuessingGame();
-                return true;
             } else if (result == "3"){
                 return false;
             } else {
                 Console.WriteLine("Invalid input");
-                return true;
-            }
+            } return true;
         }
 
         private static void PrintNumbers() {
+            Console.Clear();
             Console.WriteLine("Print Numbers");
+            Console.Write("Type a number:");
+            int result = int.Parse(Console.ReadLine());
+            Console.Write("1");
+            int counter = 2;
+            while(counter < result + 1) {
+                Console.Write("-" + counter);
+                counter++;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to Continue");
             Console.ReadLine();
         }
         private static void GuessingGame() {
+            Console.Clear();
             Console.WriteLine("Guessing game!");
             Console.ReadLine();
         }
